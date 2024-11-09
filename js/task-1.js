ed290feac0199c9f0c0ec39e03a917ebf046bded
Yuliia <yuliavaculchuk@gmail.com>
2024-11-09 23:28:15 +0200

    1 та 3 виправлене

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,18 @@
-const categoriesList = document.querySelectorAll('#categories .item');
-categoriesList.forEach(category => {
-  const categoryTitle = category.querySelector('h2').textContent;
-  const categoryItems = category.querySelectorAll('ul li').length;
-  console.log(categoryTitle);
-  console.log(categoryItems);
-});
+const categoryAllEl = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoryAllEl.length}`);
+
+categoryAllEl.forEach(el =>
+  console.log(
+    `Category: ${el.firstElementChild.textContent}`,
+    `\nElements: ${el.lastElementChild.children.length}`
+  )
+);
+
+//const categoriesList = document.querySelectorAll('#categories .item');
+//categoriesList.forEach(category => {
+// const categoryTitle = category.querySelector('h2').textContent;
+// const categoryItems = category.querySelectorAll('ul li').length;
+// console.log(categoryTitle);
+// console.log(categoryItems);
+//});
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,21 @@
 function getElementWidth(content, padding, border) {
   return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
 
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-nameInput.addEventListener('input', () => {
-  const inputValue = nameInput.value.trim();
-  nameOutput.textContent = inputValue === `` ? `Anonymous` : inputValue;
+//const nameInput = document.querySelector('#name-input');
+//const nameOutput = document.querySelector('#name-output');
+//nameInput.addEventListener('input', () => {
+// const inputValue = nameInput.value.trim();
+// nameOutput.textContent = inputValue === `` ? `Anonymous` : inputValue;
+//});
+
+const inputEl = document.querySelector('#name-input');
+
+const userNameOutputEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', event => {
+  userNameOutputEl.textContent =
+    event.currentTarget.value.trim() === ''
+      ? 'Anonymus'
+      : event.currentTarget.value.trim();
 });
